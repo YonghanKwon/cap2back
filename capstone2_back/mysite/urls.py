8449c8875e111e09c2cae2f6793e62baa532b3b2
YonghanKwon <80812666+YonghanKwon@users.noreply.github.com>
2023-05-19 05:41:24 +0900

    Update urls.py

--- capstone2_back/mysite/urls.py
+++ capstone2_back/mysite/urls.py
@@ -20,5 +20,4 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('polls.urls')),
-    path('discordbot/', include('discordbot.urls')),
 ]
